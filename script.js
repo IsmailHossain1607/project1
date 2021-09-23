94d6b8b71178be272341225be5524196f85a654f
IsmailHossain1607 <79390936+IsmailHossain1607@users.noreply.github.com>
2021-09-23 23:58:45 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var image = document.getElementById ("image");
+image.addEventListener("mouseover", function(){
+  this.width = "590";
+});
+
+image.addEventListener("mouseout", function(){
+  this.width = "490";
+});
